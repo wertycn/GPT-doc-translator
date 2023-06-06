bf148baf705733e9222381054ff8e0c9e31e96d2
debug.icu <hajnxg@126.com>
2023-06-07 00:25:39 +0800

    fix 修复失败的单元测试

--- document_process.py
+++ document_process.py
@@ -3,8 +3,9 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Any, List
 import nbformat
-from nbformat.v4 import new_notebook, new_code_cell, new_markdown_cell
-
+from docutils import nodes
+from docutils.parsers.rst import Parser
+from docutils.utils import new_document
 
 @dataclass
 class TranslateContext:
@@ -125,22 +126,24 @@ class MarkdownProcessor(DocumentProcessor):
 class NotebookProcessor(DocumentProcessor):
     def read_document(self, filepath):
         with open(filepath, 'r', encoding='utf-8') as f:
-            return nbformat.read(f, as_version=4)
+            return f.read()
 
-    def split_document(self, document: nbformat.notebooknode.NotebookNode, max_length: int) -> List[DocumentPiece]:
+    def split_document(self, document: str, max_length: int) -> List[DocumentPiece]:
+        notebook = nbformat.reads(document, as_version=4)
         pieces = []
-        for cell in document.cells:
+
+        for cell in notebook.cells:
             if cell.cell_type == "markdown":
-                # Split Markdown cell into pieces
-                markdown_pieces = self._split_markdown_cell(cell, max_length)
-                pieces.extend(markdown_pieces)
+                pieces.extend(self._split_markdown_cell(cell, max_length))
             elif cell.cell_type == "code":
-                # Treat code cell as a single piece
-                pieces.append(DocumentPiece(cell.source, "code", metadata={"cell_type": "code"}))
+                pieces.append(DocumentPiece(cell.source, "code", {"cell_type": cell.cell_type}))
+            else:
+                pieces.append(DocumentPiece(cell.source, "unknown", {"cell_type": cell.cell_type}))
+
         return pieces
 
     def _split_markdown_cell(self, cell, max_length):
-        # Reuse the MarkdownProcessor's logic to split markdown text
+        # Reuse the MarkdownProcessor's logic to split Markdown text
         markdown_processor = MarkdownProcessor(self.translator, self.context)
         return markdown_processor.split_document(cell.source, max_length)
 
@@ -160,15 +163,72 @@ class NotebookProcessor(DocumentProcessor):
         return translated_pieces
 
     def combine_pieces(self, pieces):
-        nb = new_notebook()
+        translated_notebook = nbformat.v4.new_notebook()
         for piece in pieces:
-            if piece.type == "code":
-                cell = new_code_cell(piece.text)
+            if piece.type in ["code", "unknown"]:
+                cell = nbformat.v4.new_code_cell(piece.text)
             else:
-                cell = new_markdown_cell(piece.text)
-            nb.cells.append(cell)
-        return nb
+                cell = nbformat.v4.new_markdown_cell(piece.text)
+            cell.cell_type = piece.metadata.get("cell_type", cell.cell_type)
+            translated_notebook.cells.append(cell)
+
+        return nbformat.writes(translated_notebook)
 
     def save_document(self, document):
+        notebook = nbformat.reads(document, as_version=4)
         with open(self.context.target_path, 'w', encoding='utf-8') as f:
-            nbformat.write(document, f)
+            nbformat.write(notebook, f)
+
+
+
+
+
+class RSTProcessor:
+    def __init__(self):
+        self.parser = Parser()
+
+    def split_document(self, document: str, max_length: int):
+        # 创建一个新的 RST 文档对象
+        document_node = new_document('Document')
+        # 解析文档
+        self.parser.parse(document, document_node)
+
+        # 存储分割的段落
+        pieces = []
+        current_piece = ''
+        is_code = False  # 用于标记是否在处理代码块
+
+        # 遍历文档的所有子节点
+        for child in document_node.children:
+            # 我们只关心段落节点
+            if isinstance(child, nodes.paragraph):
+                # 获取段落的文本内容
+                paragraph_text = child.astext()
+
+                # 检查是否遇到了代码块的开始标记
+                if paragraph_text.strip().endswith('::'):
+                    is_code = True
+
+                # 如果是在处理代码块，或者如果添加这个段落会超过最大长度，那么就新开一个分片
+                if is_code or len(current_piece) + len(paragraph_text) > max_length:
+                    pieces.append(current_piece)
+                    current_piece = paragraph_text
+                else:
+                    # 否则，将这个段落添加到当前的分片
+                    current_piece += paragraph_text
+
+                # 如果遇到了非缩进的段落，那么就结束代码块
+                if is_code and not paragraph_text.startswith('    '):
+                    is_code = False
+
+        # 最后，记得将最后一个分片也添加到结果中
+        pieces.append(current_piece)
+
+        return pieces
+
+    def combine_pieces(self, pieces: list[str]) -> str:
+        # 将所有的分片连接成一个完整的文档
+        return '\n'.join(pieces)
+
+
+
